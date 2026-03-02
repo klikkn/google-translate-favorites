fd7d2e4637319ca18785b839d22f9d61dd83b891
Sergey <9366365+klikkn@users.noreply.github.com>
2026-03-02 22:00:41 +0100

    feat: analytics

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 import { fireEvent } from './analytics.js';
 
-chrome.runtime.onMessage.addListener((message) => {
+chrome.runtime.onMessage.addListener((message) => {  
   if (message.action === 'track_event') {
     fireEvent(message.category, {
       label: message.label
